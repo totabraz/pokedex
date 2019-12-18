d2c01be9f829c5ff34a314b67ad189273f3bd029
Tota <hey@totabraz.com>
2019-12-18 14:30:05 -0300

    refactoring the profile and pokeinfo

--- src/containers/Pokeinfo/Pokeinfo.js
+++ src/containers/Pokeinfo/Pokeinfo.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import queryString from 'query-string';
 import * as pokedexActions from '../../store/actions/pokedexActions'
 import Profile from '../../components/profile/profile'
-import classes from './Pokeinfo.module.scss'
 
 class Pokeinfo extends Component {
 
@@ -47,7 +46,11 @@ class Pokeinfo extends Component {
 
     
     setPokemon = () => {
-
+        let params = queryString.parse(this.props.location.search)
+        if (params.id) {
+            const pokemon = this.props.pokemons.filter(pokemon => pokemon.Number === params.id)
+            return  (pokemon[0])? pokemon[0] : null;
+        }
     }
 
     render() {
@@ -55,15 +58,15 @@ class Pokeinfo extends Component {
         let nextEvolutions = null
         let prevEvolutions = null
         if (this.props.pokemons) {
-            let params = queryString.parse(this.props.location.search)
-            const pokemon = this.props.pokemons.filter(pokemon => pokemon.Number === params.id)
-            profile =  <Profile infos={pokemon[0]} />
-            console.log(pokemon[0])
-            prevEvolutions = this.setPrevEvolutions(pokemon[0], this.props.pokemons)
-            nextEvolutions = this.setNextEvolutions(pokemon[0], this.props.pokemons)
+            const pokemon = this.setPokemon()
+            if (pokemon){
+                profile =  <Profile infos={pokemon} />
+                prevEvolutions = this.setPrevEvolutions(pokemon, this.props.pokemons)
+                nextEvolutions = this.setNextEvolutions(pokemon, this.props.pokemons)
+            }
         }
         return (
-            <div className={classes.Pokeinfo}>
+            <div>
                 {profile}
                 {prevEvolutions}
                 {nextEvolutions}
@@ -72,41 +75,12 @@ class Pokeinfo extends Component {
     }
 }
 
-
-
-/**
-
-About: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
-Base Attack: "118 attack points."
-Base Defense: "118 defense points."
-Base Flee Rate: "10% chance to flee."
-Base Stamina: "90 stamina points."
-Buddy Distance: "3km (Medium)"
-Fast Attack(s): (2) [{…}, {…}]
-Generation: "Generation I"
-Height: {Minimum: "0.61m", Maximum: "0.79m"}
-MaxCP: 951
-MaxHP: 1071
-Name: "Bulbasaur"
-Next Evolution Requirements: {Amount: 25, Name: "Bulbasaur candies"}
-Next evolution(s): (2) [{…}, {…}]
-Number: "001"
-Resistant: (5) ["Water", "Electric", "Grass", "Fighting", "Fairy"]
-Special Attack(s): (3) [{…}, {…}, {…}]
-Types: (2) ["Grass", "Poison"]
-Weaknesses: (4) ["Fire", "Ice", "Flying", "Psychic"]
-Weight: {Minimum: "6.04kg", Maximum: "7.76kg"}
-*/
-
-
-
 const mapStateToProps = state => {
     return {
         pokemons: state.pokemons
 
     }
 }
-
 
 const mapDispatchToProps = dispatch => {
     return {
